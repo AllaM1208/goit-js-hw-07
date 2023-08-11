3c702e400c35ecac9c47218d349b71c223b48b51
AllaM1208 <tionqzmi@gmail.com>
2023-08-12 02:07:15 +0400

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(item => `<div class="gallery__item">
+                  <a class="gallery__link" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                  </a>
+                </div>`)
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = basicLightbox.create(`<div class="lightbox"><img src="" alt="" /></div>`);
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const original = event.target.parentNode.href;
+    const description = event.target.alt;
+    lightbox.element().querySelector('img').src = original;
+    lightbox.element().querySelector('img').alt = description;
+    lightbox.show();
+  }
+});
+
+lightbox.on('show', () => {
+  document.body.style.overflow = 'hidden';
+});
+lightbox.on('close', () => {
+  document.body.style.overflow = '';
+});
